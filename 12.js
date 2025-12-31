370eea8542fac0e6642c68abb1aba86e8499c063
Talha ahmad awan <zablerkhanahmad@gmail.com>
2025-12-31 17:01:55 +0500

    theory of how javascript works

---
+++ 12.js
@@ -0,0 +1,53 @@
+// part 2 learning javascript
+
+
+console.log(`// how javascript works
+
+// 1 > compilation phase
+//     1 > tokenizng or lexing
+//     to make it like small chanks 
+//     2 > parsing 
+//     in parsing that chanks are understanded and 
+//     a abstract syntac tree AST is maked 
+//     throug which our code is in form of executable FormData
+//     3 > code generation 
+//     the code is generated 
+
+ // why compalition
+// to understand which variable belong and where do it work 
+
+
+// in compilation phase
+// -> early error checking 
+// -> determine appropriate scope for variable 
+
+
+// 2 >  code execution phase
+// IN Js code executes inside the execution context 
+`);
+
+// how javascript works
+
+// 1 > compilation phase
+//     1 > tokenizng or lexing
+//     to make it like small chanks 
+//     2 > parsing 
+//     in parsing that chanks are understanded and 
+//     a abstract syntac tree AST is maked 
+//     throug which our code is in form of executable FormData
+//     3 > code generation 
+//     the code is generated 
+
+ // why compalition
+// to understand which variable belong and where do it work 
+
+
+// in compilation phase
+// -> early error checking 
+// -> determine appropriate scope for variable 
+
+
+// 2 >  code execution phase
+// IN Js code executes inside the execution context 
+
+
