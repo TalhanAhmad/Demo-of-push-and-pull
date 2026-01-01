1ad269cfec5bf18ff5b37df3e0e7c9784a9ee41e
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-01-01 15:54:34 +0500

    new files

--- 13.js
+++ 13.js
@@ -1,3 +1,15 @@
 // select elements by get elements by id
-const signup =document.getElementById("main todo list");
+const signup =document.getElementById("main-todo-List");
 console.log(signup);
+
+// select elements by using query selector
+const mainheading = document.querySelector("#main-todo-List");
+console.log(mainheading);
+
+const todocontainer = document.querySelector(".talha");
+console.log(todocontainer);
+
+// select elements by using queryselecterAll
+const number = document.querySelectorAll(".navitem");
+console.log(number);
+
---
+++ 14.js
@@ -0,0 +1,11 @@
+// change text
+// textcontent and innertext
+
+
+
+const mainHeading = document.getElementById("main");
+console.log(mainHeading.innerText);
+// mainHeading.textContent = "my updated todo list"
+
+
+
