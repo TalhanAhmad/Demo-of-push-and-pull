60b7d407f54f07dcf0f24fe85fcabac05f347be1
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-01-01 14:41:48 +0500

    css and html added

--- 12.js
+++ 12.js
@@ -1,30 +1,6 @@
 // part 2 learning javascript
 
 
-console.log(`// how javascript works
-
-// 1 > compilation phase
-//     1 > tokenizng or lexing
-//     to make it like small chanks 
-//     2 > parsing 
-//     in parsing that chanks are understanded and 
-//     a abstract syntac tree AST is maked 
-//     throug which our code is in form of executable FormData
-//     3 > code generation 
-//     the code is generated 
-
- // why compalition
-// to understand which variable belong and where do it work 
-
-
-// in compilation phase
-// -> early error checking 
-// -> determine appropriate scope for variable 
-
-
-// 2 >  code execution phase
-// IN Js code executes inside the execution context 
-`);
 
 // how javascript works
 
@@ -49,5 +25,10 @@ console.log(`// how javascript works
 
 // 2 >  code execution phase
 // IN Js code executes inside the execution context 
-
+// there are two types of execution context
+// 1 > global execution context 
+    // 1.>creation phase 
+    // 1.>code execution phase
+// 2 > functional execution context
+            
 
---
+++ 13.js
@@ -0,0 +1,3 @@
+// select elements by get elements by id
+const signup =document.getElementById("main todo list");
+console.log(signup);
