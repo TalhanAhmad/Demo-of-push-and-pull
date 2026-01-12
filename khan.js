e89b5852293e6fc8af61e7286abb2814f39be810
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-01-12 14:35:36 +0500

    modified

--- khan.js
+++ khan.js
@@ -1,24 +1,47 @@
-const khawali = document.querySelector("button");
+// const khawali = document.querySelector("button");
+// const body = document.body;
+// const currentColor = document.querySelector(".current-color");
+// console.log(currentColor);
+
+
+
+
+// function randomColorGenerator() {
+//     const red = Math.floor(Math.random() * 256);
+//     const green = Math.floor(Math.random() * 256);
+//     const blue= Math.floor(Math.random() * 256);
+//     const randomColor = `rgb(${red},${green},${blue})`;
+//     return randomColor ;
+// }
+
+
+// khawali.addEventListener("click",()=>{
+//    const randomColor = randomColorGenerator();
+//   body.style.backgroundColor = randomColor;
+//    currentColor.textContent = randomColor;
+
+    
+// })
+
+
+
+const khanwali = document.querySelector("button");
 const body = document.body;
 const currentColor = document.querySelector(".current-color");
 console.log(currentColor);
 
-
-
-
 function randomColorGenerator() {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue= Math.floor(Math.random() * 256);
-    const randomColor = `rgb(${red},${green},${blue})`;
-    return randomColor ;
+    const red=  Math.floor(Math.random()*251);
+    const  green=  Math.floor(Math.random()*252);
+    const blue=  Math.floor(Math.random()*250);
+    const randomColor = `rgb(${red},${green},${blue})`
+    return randomColor;
+
 }
 
-
-khawali.addEventListener("click",()=>{
-   const randomColor = randomColorGenerator();
-  body.style.backgroundColor = randomColor;
-   currentColor.textContent = randomColor;
-
+khanwali.addEventListener("click",()=>{
+    const randomColor = randomColorGenerator();
+    body.style.backgroundColor = randomColor;
+    currentColor.textContent = randomColor;
     
 })
