b784a6ba75161af76c9483f9d9b96721f27d2a57
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-01-15 15:24:59 +0500

    promise chaining

---
+++ 25.js
@@ -0,0 +1,35 @@
+// promise.resolve
+// promise chaining
+
+
+
+const myPromise = Promise.resolve(5);
+myPromise.then(value=>{
+    console.log(value);
+})
+
+// then()
+// then() method always return promise is ka faida ye ha ka ham s
+// is sa promise ki chain produce kar sakte hain
+
+
+function uPromise(){
+    return new Promise((resolve, reject) => {
+        resolve("foo");
+    })
+}
+
+uPromise().then((value)=>{
+    console.log(value);
+    value += "bar";
+    return value;
+    
+}).then((value)=>{
+    console.log(value);
+    value += "boooz";
+    return value;
+    
+}).then((value)=>{
+    console.log(value);
+    
+})
