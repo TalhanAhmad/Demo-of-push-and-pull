157b44b292cb8139b211eaaf659bfbf358a6037e
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-01-15 15:07:56 +0500

    settimeour and promise

---
+++ 24.js
@@ -0,0 +1,22 @@
+// promise and setTimmeout 
+
+
+function myPromise(){
+    return new Promise((resolve, reject) => {
+        const value = false ;
+        setTimeout(()=>{
+            if (value) {
+                resolve();
+            }else{
+                reject();
+            }
+        },1000);
+    })
+}
+
+myPromise().then(()=>{
+    console.log("resolved")
+    
+    
+}).catch(()=>{console.log("rejected");
+});
