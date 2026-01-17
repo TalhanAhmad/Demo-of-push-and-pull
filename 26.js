4b54d2a3196f31d8f846b300286dd84736b184e1
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-01-17 17:25:25 +0500

    added

---
+++ 26.js
@@ -0,0 +1,14 @@
+const xhr = new XMLHttpRequest();
+function myPromise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                resolve(xhr.responseText);
+            } else if (xhr.readyState === 4) {
+                reject(new Error("Request failed"));
+            }
+        };
+        xhr.open("GET", "https://api.example.com/data");
+        xhr.send();
+    });
+})
